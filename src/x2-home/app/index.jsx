68690ff4aa31ca9b9656ccb670acafbf24d3690d
Henry Li <henry1943@163.com>
2018-07-29 16:30:33 +0800

    Add demo app

---
+++ src/x2-home/app/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+class App extends React.PureComponent {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  render() {
+    return <div>X4 Home App</div>;
+  }
+}
+
+export default hot(module)(App);
--- src/x2-home/index.jsx
+++ src/x2-home/index.jsx
@@ -1,14 +1,1 @@
-import { hot } from 'react-hot-loader';
-import React from 'react';
-
-export class App extends React.PureComponent {
-  static propTypes = {};
-
-  static defaultProps = {};
-
-  render() {
-    return <div>X4 Home</div>;
-  }
-}
-
-export default hot(module)(App);
+export { default } from './app';
